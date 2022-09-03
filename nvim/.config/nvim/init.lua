6b86b64ae344ba252134ed48c218995112d6043b
BABA983 <692877906@qq.com>
2022-09-03 18:44:40 +0800

    feat: use GNU stow to manage my dotfiles

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,23 @@
+-- if (not vim.g.vscode) then
+  -- cache
+  require "user.impatient"
+  require "user.options"
+  require "user.keymaps"
+  require "user.plugins"
+  require "user.theme"
+  require "user.cmp"
+  require "user.lsp"
+  require "user.telescope"
+  require "user.treesitter"
+  require "user.autopairs"
+  require "user.comment"
+  require "user.gitsigns"
+  require "user.nvim-tree"
+  require "user.bufferline"
+  require "user.lualine"
+  require "user.toggleterm"
+  require "user.project"
+  require "user.indent-line"
+  require "user.alpha"
+  require "user.which-key"
+-- end
