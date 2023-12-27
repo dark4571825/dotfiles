54f91a1ec65a83387f27d99e7408dd7b799518c0
BABA983 <692877906@qq.com>
2023-12-27 18:21:30 +0800

    add wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require("wezterm")
+
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.font = wezterm.font("Hack Nerd Font")
+config.color_scheme = "Solarized (dark) (terminal.sexy)"
+
+return config
